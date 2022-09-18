aa5d9e92061f625c5f2f377847465502124026b7
amir <aamir4ly@gmail.com>
2022-09-18 10:14:50 +0530

    initial commit

---
+++ TDD/Book - Saleem Siddiqui/js/portfolio.js
@@ -0,0 +1,31 @@
+const Money = require("./money");
+
+
+class Portfolio {
+    
+    constructor() {
+        this.moneys = [];
+    };
+
+    add(...moneys) {
+        this.moneys = this.moneys.concat(moneys);
+    };
+
+    convert(money, currency) {
+        const eurToUsd = 1.2;
+        if (money.currency == currency) {
+            return money.amount;
+        };
+        return money.amount * eurToUsd;
+    };
+
+    evaluate(currency) {
+        let total = this.moneys.reduce((sum, money)=> {
+            return sum + this.convert(money, currency);
+        }, 0);
+        return new Money(total, currency);
+    };
+};
+
+module.exports = Portfolio;
+
